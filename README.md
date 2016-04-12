4f5a091988ccaa69aa0735133447b5b2f3b504b1
James Fairhurst <info@jamesfairhurst.co.uk>
2016-04-12 16:08:04 +0100

    Initial commit
    Included lambda function for AWS

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # laravel-cloudwatch-logs-lambda-trello
+Use AWS CloudWatch Logs &amp; Lambda to post Laravel Log messages to Trello.
+
+![All Log Levels](screenshot-all-log-levels.png)
+
+![Exception Info](screenshot-exception.png)
+
+# Setup
+## Step 1 - Get Your Laravel Logs into CloudWatch
+I've written [an article](https://medium.com/@james_fairhurst/using-aws-cloudwatch-for-laravel-logs-on-forge-27590ee4fe33) which goes through the process of getting your Laravel app logs into CloudWatch. It's *relatively* straight forward however like everything it depends on your experience. Hit me up if you're having trouble.
+
+## Step 2 - Get Your Trello App Key & User Token
+Get your [App Key](https://trello.com/app-key) and also click on the **Token** link to generate a User Token. Input these into the variables in the **index.js** file e.g.
+
+```
+trelloApiKey = 'xxx';
+trelloToken  = 'xxx';
+```
+
+## Step 3 - Find Your Trello List ID
+Grab the Trello Board ID from the URL when viewing it e.g. `https://trello.com/b/BOARD_ID/personal` and then plug in all the variables into the URL below to get all the List IDs in the Board.
+
+```
+https://api.trello.com/1/boards/BOARD_ID/lists?key=APP_KEY&token=TOKEN
+```
+
+You'll get a JSON response and you'll have to grab the List ID of the List you want to create Cards in.
+
+```
+[{"id":"LIST_ID","name":"To Do"...]
+```
+
+A bit long-winded but finally input the List ID into the variable in the **index.js** file e.g.
+
+```
+trelloListId = 'xxx';
+```
+
+## Step 4 - Create a Lambda Function
+Create a new Lambda function in the AWS Console and select the **cloudwatch-logs-process-data** blueprint as a starting point, this will ask you to select a Log Group (your Laravel log) and an optional pattern. Name your function and copy & paste the code in **index.js** into the inline code editor (remember to update the **Trello** variables). I've created a more detailed walkthrough in [an article]().
+
+# Summary
+Although there are other ways to get notified of errors e.g. external services ([Bugsnag](https://bugsnag.com/)) or Laravel specific packages ([lern](https://github.com/tylercd100/lern)) depending on your circumstances this could be another option. I've been wanting to delve into & learn the AWS eco-system for some time now and I've been documenting my efforts on Medium to hopefully help others & future me when I come to do things again. I'm also a Nodejs noob so if things can be improved let me know!
+
+# Todo
+* Stop duplicate Cards from being created
+* Option to exclude log levels e.g. debug & info so only proper errors are reported
+* ~~Article to document the Trello & Lambda setup steps~~ [Using AWS CloudWatch & Lambda to create Trello Cards from Laravel Logs]()
---
+++ index.js
@@ -0,0 +1,91 @@
+var zlib = require('zlib');
+var https = require('https');
+var querystring = require('querystring');
+var trelloApiKey, trelloToken, trelloListId, totalRequests, completedRequests;
+
+trelloApiKey = '';
+trelloToken  = '';
+trelloListId = '';
+
+function processLogEvent(logEvent, context) {
+    var date = new Date(logEvent.timestamp);
+
+    // Nasty way to grab log type & message (need to learn regular expressions!!)
+    var split1 = logEvent.message.split(']');
+    var split2 = split1[1].trim().split(':');
+    var split3 = split2[0].split('.');
+
+    var logTitle = split2[0].trim();
+    var logMessage = logEvent.message.split('\n')[0].trim();
+    var logType = split3[1];
+
+    postToTrello(logTitle, logMessage, logType, context);
+}
+
+function postToTrello(logTitle, logMessage, logType, context) {
+    var payloadStr = {
+        "idList": trelloListId,
+        "name": logTitle,
+        "due": null,
+        "urlSource": null,
+    };
+
+    var postData = querystring.stringify(payloadStr);
+
+    var options = {
+        hostname: 'api.trello.com',
+        port: 443,
+        path: '/1/cards?key=' + trelloApiKey + '&token=' + trelloToken,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postData.length
+        }
+    };
+
+    var postReq = https.request(options, function(res) {
+        var chunks = [];
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            return chunks.push(chunk);
+        });
+        res.on('end', function() {
+            var body = chunks.join('');
+
+            if (res.statusCode < 400) {
+                console.info('Message posted successfully');
+            } else if (res.statusCode < 500) {
+                console.error("Error posting message to Trello API: " + res.statusCode + " - " + res.statusMessage);
+            } else {
+                console.error("Server error when processing message: " + res.statusCode + " - " + res.statusMessage);
+            }
+
+            if (completedRequests++ == totalRequests - 1) {
+                context.succeed('DONE');
+            }
+        });
+        return res;
+    });
+
+    postReq.write(postData);
+    postReq.end();
+}
+
+exports.handler = function(event, context) {
+    var payload = new Buffer(event.awslogs.data, 'base64');
+    zlib.gunzip(payload, function(e, result) {
+        if (e) {
+            context.fail(e);
+        } else {
+            result = JSON.parse(result.toString('utf8'));
+            console.log("Decoded payload: ", JSON.stringify(result));
+
+            completedRequests = 0;
+            totalRequests = result.logEvents.length;
+
+            result.logEvents.forEach(function (logEvent) {
+                processLogEvent(logEvent, context);
+            });
+        }
+    });
+};
